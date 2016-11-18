e89d7296e618f764fbfd73649ec746c397d78b28
David Feldbaum <Dfeldbaum@Davids-MacBook-Pro-2.local>
2016-11-18 11:59:25 -0600

    updated with lyric REST

--- controllers/lyrics_controller.rb
+++ controllers/lyrics_controller.rb
@@ -14,23 +14,66 @@ class LyricsController < ApplicationController
 
 
 
-	# post '/createlyrics' do
+	post '/createlyrics' do
 
-	# 	@artist = params[:artist]
-	# 	@song = params[:song]
-	# 	@lyrics = params[:lyrics]
-	# 	@year = params[:year]
-	# 	@genre = params[:genre]
-	# 	@image = params[:image]
-	# 	@user_id = params[:user_id]
-
-
-	# 	@model = Lyric.new
+		@artist = params[:artist]
+		@song = params[:song]
+		@lyrics = params[:lyrics]
+		@year = params[:year]
+		@genre = params[:genre]
+		@image = params[:image]
+		@user_id = params[:user_id]
 
 
+		@model = Lyrics.new
+		@model.artist = @artist
+		@model.song = @song
+		@model.lyrics = @lyrics
+		@model.year = @year
+		@model.genre = @genre
+		@model.image = @genre
+		@model.user_id = @genre
+		@model.save
 
-	# end
-	
+		session[:user] = @model
+		@username = session[:user][:username]
+
+
+
+	end
+
+
+	patch '/:id' do
+		@id = params[:id]
+		@artist = params[:artist]
+		@song = params[:song]
+		@lyrics = params[:lyrics]
+		@year = params[:year]
+		@genre = params[:genre]
+		@image = params[:image]
+		@user_id = params[:user_id]
+
+
+
+		@model = Lyric.find(@id)
+		@model.artist = @artist
+		@model.song = @song
+		@model.lyrics = @lyrics
+		@model.year = @year
+		@model.genre = @genre
+		@model.image = @genre
+		@model.user_id = @genre
+		@model.save
+
+		session[:user] = @model
+		@username = session[:user][:username]
+
+
+
+	end
+
+
+
 
 
 end		
